496d6523c3f159112944843d94445d2620c3ec5a
Sanjh Parwani <simi.sanjh3@gmail.com>
2021-09-08 00:42:50 +0530

    setup react basic code using cra

--- src/tests/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../components/App";
-
-test("renders sadaf banning", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/sadaf/i);
-  expect(linkElement).toBeInTheDocument();
-});
